a3971444621b185ef9b2e22b7005e213f39b17ac
Alexey Sosnovsky <lvlonstradamus@gmail.com>
2015-07-21 11:51:49 +0300

    js - прописал основные роуты

--- builds/dev/app/main.js
+++ builds/dev/app/main.js
@@ -1,3 +1,29 @@
 ;(function(){
+    'use strict';
+
+    angular
+        .module('fitness',['ui.router'])
+        .config(FitnessConfig);
+
+    function FitnessConfig ($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home',{
+                url: '/',
+                templateUrl: 'app/home/home.html'
+            })
+            .state('about',{
+                url:'/about',
+                templateUrl: 'app/about/about.html'
+            })
+            .state('contacts',{
+                url:'/contacts',
+                templateUrl: 'app/contacts/contacts.html'
+            })
+            .state('profile',{
+                url:'/profile',
+                templateUrl: 'app/profile/profile.html'
+            })
+    }
 
 })();
