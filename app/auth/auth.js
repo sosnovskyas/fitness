6a4bf90731bd9240f1d22498cfbd98fb595dd90d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-07-31 14:20:56 +0300

    js - added auth

---
+++ app/auth/auth.js
@@ -0,0 +1,104 @@
+;
+(function(){
+    'use strict';
+    angular
+        .module('fitness.auth', [
+            'ui.router',
+            'fitness.fire'
+        ])
+        .factory('Authentication', AuthenticationFactory)
+        .config(configAuth)
+        .controller('RegisterCtrl',RegisterController)
+        .controller('LoginCtrl',LoginController);
+
+    // @ngInject
+    function AuthenticationFactory(DBC){
+        var ref = DBC.getRef();
+        var usersRef = ref.child('users');
+        var o = {};
+
+        function authHandler(error, authData){
+            if(error){
+                console.warn('Login FAILED!', error)
+            }else{
+                console.log('Login success!', authData)
+            }
+        }
+
+        o.login = function (_user, _authHndl){
+            // если не задан то переопределяется
+            var authHndl = typeof _authHndl !== "undefined" ? _authHndl : authHandler;
+
+            DBC.get$Auth()
+                .$authWithPassword({
+                    email: "email",
+                    password : "password"
+                })
+                .then(authHndl);
+        };/*login*/
+
+        o.logout = function(){
+            ref.unauth();
+        };
+
+        o.register = function(_user){
+            DBC.get$Auth()
+                .$createUser({
+                    email : _user.email,
+                    password : _user.password
+                })
+                .then(function(userData){
+                    consple.log('User '+ userData.uid + ' created successfully!');
+                    var userRef = usersRef.child(userData.uid);
+                    userRef.set({
+                        fullname: _user.fullname,
+                        date: Firebase.ServerValue.TIMESTAMP
+                    });
+                })
+        };
+
+        return o;
+    }
+
+    // @ngInject
+    function configAuth($stateProvider){
+        $stateProvider
+            .state('register', {
+                url: '/register',
+                templateUrl: 'auth/registration.html',
+                contioller: 'RegisterCtrl',
+                controllerAs: 'rc'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'auth/login.html',
+                contioller: 'LoginCtrl',
+                controllerAs: 'lc'
+            })
+    }
+
+    // @ngInject
+    function RegisterController (Authentication){
+        var s = this;
+        s.user = {
+            fullname: 'Qwe Qwe',
+            email: 'qwe@qwe.ru',
+            password: '1234'
+        };
+        s.register  = function (){
+            Authentication.register(s.user);
+        }
+    }
+
+    // @ngInject
+    function LoginController (Authentication) {
+        var s = this;
+        s.user = {
+            email: 'qwe@qwe.ru',
+            password: '1234'
+        };
+        s.login = function () {
+            Authentication.login(s.user);
+        }
+    }
+})();
--- app/userSpace/userSpace.js
+++ app/userSpace/userSpace.js
@@ -3,10 +3,12 @@
     angular
         .module('fitness.userSpace', [
             'fitness.userSpaceWorkout',
-            'fitness.userSpaceExercises'
-
+            'fitness.userSpaceExercises',
+            'fitness.fire'
         ])
-        .config(FitnessUserSpaceConfig);
+        .config(FitnessUserSpaceConfig)
+        .controller('userSpaceCtrl',userSpaceController);
+
 
     function FitnessUserSpaceConfig($stateProvider, $urlRouterProvider){
         $urlRouterProvider
@@ -18,4 +20,19 @@
             })
 
     }
+
+    function userSpaceController(){
+        var ref = new Firebase("https://ngfitness.firebaseio.com");
+        ref.authWithPassword({
+            email    : "lvlonstradamus@gmail.com",
+            password : "123123"
+        }, function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+            }
+        });
+    }
+
 })();
