3211e42ebf53a9d4414a95a03fbe073bd104c713
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-08-13 12:32:00 +0300

    refact

--- gulp/tasks/dev-watch.js
+++ gulp/tasks/dev-watch.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp');
 var config = require('../config').devWatch;
 
-gulp.task('dev-watch', function(){
-    gulp.watch('src/**/*.js',['dev-js']);
-    gulp.watch('src/**/*.scss',['dev-css']);
-    gulp.watch('src/**/*.jade',['dev-markup']);
+gulp.task('dev-watch', function () {
+    gulp.watch('src/**/*.js', ['dev-js']);
+    gulp.watch('src/**/*.scss', ['dev-css-custom']);
+    gulp.watch('src/**/*.jade', ['dev-markup']);
 });
-
-
