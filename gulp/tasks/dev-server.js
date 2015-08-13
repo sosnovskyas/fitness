932cda577e23131df245cd4fbcb83e7eccd758e7
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-08-13 12:31:34 +0300

    refact - change to synk task run

--- gulp/tasks/dev-server.js
+++ gulp/tasks/dev-server.js
@@ -3,10 +3,10 @@ var config = require('../config').devServer;
 
 var webserver = require('gulp-webserver');
 
-gulp.task('dev-server', function(){
-    gulp.src(config.dest)
+gulp.task('dev-server', ['dev-js','dev-css','dev-img','dev-markup'], function () {
+    return gulp.src(config.dest)
         .pipe(webserver({
             livereload: true,
             open: true
-        }))
+        }));
 });
