f89d12b444163f5d33194033d22d06ebd23afde0
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-08-14 13:57:52 +0300

    js - added new directive

--- src/private/userSpace.js
+++ src/private/userSpace.js
@@ -7,13 +7,42 @@
         ])
         .config(FitnessUserSpaceConfig)
         .filter('toRub', toRublesFilter)
+        .directive('userBar', userBarDirective)
      ;
+    // @ngInject
+    function userBarDirective() {
+        return {
+            //compile: function compile(temaplateElement, templateAttrs) {
+            //    return {
+            //        pre: function (scope, element, attrs) {
+            //        },
+            //        post: function (scope, element, attrs) {
+            //        }
+            //    };
+            //},
+            //link: function (scope, element, attrs) {
+            //
+            //},
+            priority: 0,
+            //terminal: false,
+            templateUrl: 'private/userBar/userBar.html',
+            //replace: false,
+            //transclude: false,
+            //restrict: 'A',
+            //scope: false,
+            controller: function ($scope, $element, $attrs, $transclude, otherInjectables) {
+            }
+        };
+    }
 
+    // @ngInject
     function toRublesFilter() {
         return function (input) {
             return String(Math.floor(input * 100) / 100).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ')  + ' руб';
         };
     }
+
+    // @ngInject
     function FitnessUserSpaceConfig($stateProvider, $urlRouterProvider) {
         $urlRouterProvider
             .when('/userSpace', '/userSpace/workout');
